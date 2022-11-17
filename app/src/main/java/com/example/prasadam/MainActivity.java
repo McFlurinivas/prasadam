b56b499b0d6eab43ab591e42e8a826d1a1a87ff1
Harinivas Srinivasan <sharinivas04@gmail.com>
2022-11-17 17:38:09 +0530

    First

---
+++ app/src/main/java/com/example/prasadam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.prasadam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
